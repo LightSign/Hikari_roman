030e19c377d7786f6c7149d2c1ffdfcd4ce01757
LightSign <donlivmi@gmail.com>
2018-07-07 16:48:42 +0900

    server

--- server.py
+++ server.py
@@ -50,8 +50,8 @@ def _make_data():
 
         options = Options()
         ### for local pc
-        options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
-        # options.binary_location = '/app/.apt/usr/bin/google-chrome'
+        # options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+        options.binary_location = '/app/.apt/usr/bin/google-chrome'
         # options.add_argument('--disable-gpu')
         # options.add_argument("--no-sandbox")
         # options.add_argument('headless')
