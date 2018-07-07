9ba8d70ea0e4fb14ae9f17e7ba0ae5a131d5d735
LightSign <donlivmi@gmail.com>
2018-07-07 18:01:59 +0900

    add

--- server.py
+++ server.py
@@ -54,11 +54,11 @@ def _make_data():
         ### for local pc
         # options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
         options.binary_location = '/app/.apt/usr/bin/google-chrome'
-        # options.add_argument('--disable-gpu')
-        # options.add_argument("--no-sandbox")
-        # options.add_argument('headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument("--no-sandbox")
+        options.add_argument('headless')
         options.add_argument('window-size=1200x600')
-        # browser = webdriver.Chrome(chrome_options=options)
+
         browser = webdriver.Chrome(chrome_options=options)
         browser.implicitly_wait(20)
 
