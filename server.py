2b8a32d1c6f81fbf27c4865760c0a59a4f4c1938
LightSign <donlivmi@gmail.com>
2018-07-07 23:36:17 +0900

    ser

--- server.py
+++ server.py
@@ -98,8 +98,8 @@ def _make_data():
             print("make file")
             csv_file = cStringIO.StringIO()
             writer = csv.writer(csv_file, quoting=csv.QUOTE_NONE, delimiter=',', quotechar=',')
-            writer.writerow(data)
-            # writer.writerows(data.values)
+            writer.writerows(data)
+            #writer.writerows(data.values)
             return csv_file.getvalue()
 
         def make_csv(file_csv):
