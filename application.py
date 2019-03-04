7ca554112f030184cddb8e0cd02b0d7ca80f2f68
LightSign <donlivmi@gmail.com>
2019-03-04 14:20:20 +0900

    make amazon scray

--- application.py
+++ application.py
@@ -1,23 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, redirect, url_for , make_response,Response
 from flask_cors import CORS, cross_origin
-from datetime import datetime,timedelta
-import subprocess
-import pandas as pd
-import numpy as np
-from pandas import Series,DataFrame
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from datetime import datetime,timedelta
-from time import sleep
-import re
-import random
-import codecs
-import csv
-import io as cStringIO
-from bs4 import BeautifulSoup as bs
-import requests
+from datetime import datetime
+from processing import processing
 
 # 自身の名称を app という名前でインスタンス化する
 app = Flask(__name__)
@@ -27,76 +12,21 @@ CORS(app)
 # index にアクセスしたときの処理
 @app.route('/')
 def index():
-    title = "ATOM : 実行ページ"
+    title = "データ収集屋さん"
     return render_template('index.html',title=title)
 
 #/post にアクセスしたときの処理
 @app.route('/post', methods=['GET', 'POST'])
 def _make_data():
-    title = "ATOM : 結果ページ"
     if request.method == 'POST':
-        # リクエストフォームから「名前」を取得して
-        # ebay_id = request.form['ebay_id']
-        # ebay_pass = request.form['ebay_pass']
-        # query = request.form['search_keyword']
-        # price_min = int(request.form['price_lower'])
-        # price_max = int(request.form['price_upper'])
-        # search_weight = request.form['search_weight']
-        # tmp1 = request.form['tmp1']
-        # tmp2 = request.form['tmp2']
-        # tmp3 = request.form['tmp3']
         URL = request.form['URL']
-
-        """
-        書き込みデータ作成
-        """
-        headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0",}
-        df = pd.DataFrame(index=[] , columns=[])
-        req = requests.get(URL ,headers=headers)
-        all_html = bs(req.content,"lxml")
-        posts = all_html.select(".items-box")
-        for post in posts:
-            title = post.select("h3.items-box-name")[0].getText()
-            price = post.select(".items-box-price")[0].getText()
-            price = price.replace('¥', '').replace(",","")
-            sold = 0
-            if len(post.select(".item-sold-out-badge")) > 0:
-                sold = 1
-            url = post.a.get("href")
-            se = pd.Series([title, price, sold,url],['title','price','sold','url'])
-            df = df.append(se, ignore_index=True)
-        df["title"] = df["title"].str.replace(r"\W"," ")
-
-        ### ヤフーファイナンス株価取得確認用
-        # https://info.finance.yahoo.co.jp/fx/
-        ### ヤフーファイナンス株価取得確認用
-
-        def _make_file(data):
-            """
-            データを CSV 形式に変換
-            """
-            csv_file = cStringIO.StringIO()
-            writer = csv.writer(csv_file, quoting=csv.QUOTE_NONE, delimiter=',', quotechar=',')
-            writer.writerow(data)
-            writer.writerows(data.values)
-            return csv_file.getvalue()
-
-        def make_csv(file_csv):
-            """
-            CSV 出力
-            """
-            response = make_response()
-            response.data = file_csv
-            response.headers['Content-Type'] = 'application/octet-stream'
-            response.headers['Content-Disposition'] = 'attachment; filename=ATOM_test.csv'
-            return response
-
-        file_csv = _make_file(df)
-        csv_data = make_csv(file_csv)
-        return csv_data
-
-    else:
-        # エラーなどでリダイレクトしたい場合はこんな感じで
+        """URLからスクレイピング"""
+        browser = processing.headless_browser()
+        df = processing.first_scray(URL, browser)
+        if len(df):
+            return processing.return_csv(df)
+        return "エラーです"
+    else:# エラーなどでリダイレクトしたい場合はこんな感じで
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
---
+++ processing/processing.py
@@ -0,0 +1,64 @@
+from flask import make_response
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+
+def headless_browser():
+    options = webdriver.chrome.options.Options()
+    options.add_argument('--headless')
+    browser = webdriver.Chrome(chrome_options=options ,executable_path="./chromedriver")
+    browser.set_page_load_timeout(10)
+    return browser
+
+def first_scray(URL, browser):
+    df = pd.DataFrame(index=[] , columns=[])
+    try:
+        browser.get(URL)
+        items = browser.find_elements_by_css_selector("div.s-item-container")
+        for item in items:
+            try:
+                title = item.find_element_by_css_selector("h2").text
+                item_url = item.find_element_by_css_selector("a").get_attribute("href")
+                # check whether prime or not
+                if item.find_elements_by_css_selector("i.a-icon.a-icon-jp.a-icon-prime-jp.a-icon-small.s-align-text-bottom"):
+                    prime = "prime"
+                else:
+                    prime = "no prime"
+                pic_split = item.find_element_by_css_selector("img").get_attribute("srcset").split(",")
+                pic_txt = ",".join(pic_split).split()
+                pic_list = []
+                for p_txt in pic_txt:
+                    if "jpg" in p_txt:
+                        pic_list.append(p_txt)
+                se = pd.Series([title, item_url, prime], ["title", "item_url", "prime"])
+                df = df.append(se, ignore_index=True)
+            except:
+                pass
+        df = df.head(10).reset_index(drop=True)
+        browser.quit()
+    except Exception as e:
+        pass
+    return df
+
+import csv
+import io as cStringIO
+def return_csv(data):
+    def toCSV_format(data):
+        """データを CSV 形式に変換"""
+        csv_file = cStringIO.StringIO()
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_NONE, delimiter=',', quotechar=',')
+        writer.writerow(data)
+        writer.writerows(data.values)
+        return csv_file.getvalue()
+
+    def make_csv(csv_format_df):
+        """CSV 出力"""
+        response = make_response()
+        response.data = csv_format_df
+        response.headers['Content-Type'] = 'application/octet-stream'
+        response.headers['Content-Disposition'] = 'attachment; filename=ATOM_test.csv'
+        return response
+
+    csv_format_df = toCSV_format(data)
+    csv_data = make_csv(csv_format_df)
+    return csv_data
