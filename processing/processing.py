05b76f5442dfbdab893298c8fbce855c2c683a09
LightSign <donlivmi@gmail.com>
2019-03-04 23:18:59 +0900

    py to git

--- processing/processing.py
+++ processing/processing.py
@@ -5,15 +5,22 @@ from bs4 import BeautifulSoup as bs
 import requests
 
 def first_scray(URL):
+    # header偽装？？　いや、結論、お客さんが喜べば、それイイことだから。
     headers = {
     "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0",
     }
+    # dataframe定義しちゃうから。
     df = pd.DataFrame(index=[] , columns=[])
+    # これお客さんからのresponseで受注だから。
     res = requests.get(URL ,headers=headers)
+    # ここで、落ち着いてhtml見積書解析しちゃいますよ、と。
     soup = bs(res.content, "html.parser")
     try:
+        # ここでページ全体のコールリスト取っちゃうから。
         items = soup.select("div.s-item-container")
+        # そのリストからfor無限アポ掛けしますよ、と。
         for item in items:
+            ## 全国のコールリスト取っちゃうから。
             try:
                 title = item.select("img")[0].get("alt")
                 item_url = "https://www.amazon.co.jp" + item.select("a")[0].get("href")
@@ -32,16 +39,20 @@ def first_scray(URL):
                     ["title", "item_url", "prime", "picures_list"]
                 )
                 df = df.append(se, ignore_index=True)
+            ## リストないところはスルーして、次行くっしょ。
             except Exception as e:
                 pass
+    ## リスト取れないときは、passしちゃうってこと。
     except:
         pass
+    ## まとめたリストに早くアポ掛けしてよ。
     return df
 
 import csv
 import io as cStringIO
 import random
 def download(df):
+    # ここに名言ファイル名あるから。
     txt = """
     MoshiMoshiWatakushyyyyy
     NttnohoukaraOdenwashiteorimasu
@@ -54,11 +65,15 @@ def download(df):
     KonoyodeIppikiHikarXXXXXXXNoEigyomanHa
     """
     txt_split = txt.split()
+    # ランダムで名言出ちゃうから。　普通に。
     file_name = random.choice(txt_split)
 
+    # まあ、ここよくわかんないけど、結論大丈夫だから。
     f = cStringIO.StringIO()
     writer = csv.writer(f, quotechar='"', quoting=csv.QUOTE_ALL, lineterminator="\n")
+    # ココでカラム名指定してるから。
     writer.writerow(['item_url','pic_list','prime','title'])
+    # リスト上から全部掛けちゃうよ。
     for i in range(len(df)):
         writer.writerow([
             df.item_url[i],
@@ -66,8 +81,10 @@ def download(df):
             df.prime[i],
             df.title[i]
         ])
+    # response作ってるらしいよ。　まあアポ取れればいいでしょ。細かいことは。
     res = make_response()
     res.data = f.getvalue()
     res.headers['Content-Type'] = 'text/csv'
+    # ここで名言ファイル名csv出来ちゃいますよ、と。
     res.headers['Content-Disposition'] = 'attachment; filename='+ file_name +'.csv'
     return res
--- server.py
+++ server.py
@@ -4,30 +4,35 @@ from flask_cors import CORS, cross_origin
 from datetime import datetime
 from processing import processing
 
+# flask初期化しちゃいますよ、と。
+# apo掛けもしちゃいますよ、と。
 app = Flask(__name__)
 CORS(app)
 
-# index にアクセスしたときの処理
+# / にアクセスしたとき、処理しちゃいますよ、と。
 @app.route('/')
 def index():
-    title = "データ収集屋さん"
-    return render_template('index.html',title=title)
-    # return render_template('index1.html',title=title)
+    return render_template('index.html')
 
-#/post にアクセスしたときの処理
+#/post にアクセスしました、と。　結論、スクレイピングしますよ、と。
 @app.route('/post', methods=['GET', 'POST'])
-def _make_data():
+def make_data():
+    # POSTの処理をしますよ、と。
     if request.method == 'POST':
+        # もしHTTPSが入力されたら下記処理をしますよ、と。
         if "https" in request.form["URL"]:
             URL = request.form['URL']
-            """URLからスクレイピング"""
+            # 結論、ここでスクレイピング始めちゃうから。大丈夫エラーなんて出ないから。
             df = processing.first_scray(URL)
+            # dataframeがゼロアポじゃなかったら、CSVダウンロードしちゃいますよ、と。
             if len(df):
                 return processing.download(df)
+    # URLに空白が入力されたら詰められますよ、と。
     shinjin = "新人「光のロマンで迫るからエラーが出ましたゾス」"
     boss = "ボス「いいから早くアポ取ってこいや」"
     return render_template('back.html',shinjin=shinjin, boss=boss)
 
 if __name__ == '__main__':
-    app.debug = True # デバッグモード有効化
+    # 結論、apo掛けするから。ゼロアポなんて普通にないから。
+    # 1日500コール普通だから。500コールでゼロアポなんてないから。
     app.run()
