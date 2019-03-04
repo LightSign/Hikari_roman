35bbf2e56bab73e41c9e433473b409e2e3ac6e5b
LightSign <donlivmi@gmail.com>
2019-03-04 18:40:06 +0900

    add random filename

--- processing/processing.py
+++ processing/processing.py
@@ -15,7 +15,6 @@ def first_scray(URL):
         try:
             title = item.select("img")[0].get("alt")
             item_url = "https://www.amazon.co.jp" + item.select("a")[0].get("href")
-            # check whether prime or not
             if item.find(class_="a-icon a-icon-jp a-icon-prime-jp a-icon-small s-align-text-bottom"):
                 prime = "prime"
             else:
@@ -34,9 +33,36 @@ def first_scray(URL):
         except Exception as e:
             pass
     return df
+
 import csv
 import io as cStringIO
+import random
 def download(df):
+    txt = """
+    社歌！
+    早く円陣くめっ！
+    男のロマンで迫るなら
+    試練の道さえ怖くない
+    この世で一匹光通信（あるいは代理店名）の営業マンは
+    泣き言なんか言わないで
+    根性、根性、ド根性
+    俺がやらずに誰がやる
+    おっす完全オーダーやります
+    どっこい気合いであげてやる！
+    いいかお前ら！（おっす！）
+    昨日までのことは忘れて今日の数字を上げて
+    全員オーダーするからな！（おっす！）
+    入って来たばかりの新人も既存のメンバーも関係ねーんだよ
+    ぜってー数字にからめよ！（おっす！）
+    そん中でも、真っ先に受注あげんのはこの俺だー
+    （俺だっ、俺だっ、俺だっ、俺だっ、俺だっ、俺だっ、俺だっ…俺だっ！）
+    さらに、上乗せでアメックスのカードも社長にやらせんのはーこのー （俺だっっ！！！）
+    頑張れよ、せーの（パンッ！）（オッス！！！）
+    さー迷惑電話の開始ゾス！
+    """
+    txt_split = txt.split()
+    file_name = random.choice(txt_split)
+
     f = cStringIO.StringIO()
     writer = csv.writer(f, quotechar='"', quoting=csv.QUOTE_ALL, lineterminator="\n")
     writer.writerow(['item_url','pic_list','prime','title'])
@@ -50,5 +76,5 @@ def download(df):
     res = make_response()
     res.data = f.getvalue()
     res.headers['Content-Type'] = 'text/csv'
-    res.headers['Content-Disposition'] = 'attachment; filename='+ "test" +'.csv'
+    res.headers['Content-Disposition'] = 'attachment; filename='+ file_name +'.csv'
     return res
